c56a27b7b5e8870714eb9db893704008d3da26b2
Tasnim813 <tasnim3j@gmail.com>
2026-02-02 22:30:57 +0600

    brands logo added with swiper

---
+++ src/pages/Home/Brands/Brands.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import casion from '../../../assets/brands/casio.png'
+import amazon from '../../../assets/brands/amazon.png'
+import moonstra from '../../../assets/brands/moonstar.png'
+import star from '../../../assets/brands/star.png'
+import starPeople from '../../../assets/brands/start_people.png'
+import randstrat from '../../../assets/brands/randstad.png'
+import { Autoplay } from 'swiper/modules';
+
+const brandLogos=[casion,amazon,moonstra,star,starPeople,randstrat
+];
+const Brands = () => {
+    return (
+       <Swiper 
+       slidesPerView={4}
+        centeredSlides={true}
+        spaceBetween={30}
+        grabCursor={true} 
+        loop={true}
+
+         modules={[Autoplay]}
+          autoplay={{
+          delay: 500,
+          disableOnInteraction: false,
+        }}
+       >
+        {
+            brandLogos.map((Logo,index)=><SwiperSlide key={index}  ><img src={Logo} alt="" /></SwiperSlide>)
+        }
+       </Swiper>
+    );
+};
+
+export default Brands;
