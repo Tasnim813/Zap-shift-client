6e4e09a4ea886030185f291e243be1f58675faf8
Tasnim813 <tasnim3j@gmail.com>
2026-02-05 11:23:17 +0600

    map serch oimplement

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,30 +1,29 @@
 import React, { useRef } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'
 import { useLoaderData } from 'react-router';
+import 'leaflet/dist/leaflet.css'
 const Coverage = () => {
     const position = [23.6850, 90.3563]
-    const serviceCenter=useLoaderData()
-   const mapRef=useRef(null)
-    
-    const handleSerch=(e)=>{
-        e.preventDefault() 
-        const location =e.target.location.value;
-        const district=serviceCenter.find(c=>c.district.toLowerCase().includes(location.toLowerCase()));
+    const ServiceCenter=useLoaderData()
+    const mapRef=useRef()
+
+    const handleSearch=(e)=>{
+        e.preventDefault()
+        const location=e.target.location.value;
+        const district=ServiceCenter.find(c=>c.district.toLowerCase().includes(location.toLowerCase()));
         if(district){
-            const coord=[district.latitude,district.longitude] 
-            console.log(district,coord)
-            mapRef.current.flyTo(coord,14)
+            const coord=[district.latitude,district.longitude];
+            mapRef.current.flyTo(coord,12)
+
         }
     }
 
-
+        
     return (
         <div>
             <h1>We are available in 64 districts</h1>
             <div>
-                {/* search */}
-                <form onSubmit={handleSerch}>
+                <form onSubmit={handleSearch}>
                     <label className="input">
   <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
     <g
@@ -44,28 +43,28 @@ const Coverage = () => {
 
             </div>
             {/*  */}
-            <div className='w-full border h-[800px]'>
+            <div className='border w-full  h-[800px]'>
                 <MapContainer
                  center={position}
-                  zoom={8} 
-                  scrollWheelZoom={false}
-                  className='h-[800px]'
-                  ref={mapRef}
-                  >
+                  zoom={8}
+      scrollWheelZoom={false}
+      className='h-[800px]'
+      ref={mapRef}
+
+      >
                      <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
     {
-        serviceCenter.map((center,index)=><Marker key={index} position={[center.latitude,center.longitude]}>
-    <Popup>
-        {center.district} <br /> Service Area: {center.covered_area.join(', ')}
-    </Popup>
-
-</Marker>)
+        ServiceCenter.map((center,index)=><Marker key={index} position={[center.latitude,center.longitude
+        ]}>
+        <Popup>
+             <strong>{center.district}</strong> <br /> {center.covered_area.join(', ')}
+        </Popup>
+    </Marker>) 
     }
-
-
+    
                 </MapContainer>
             </div>
         </div>
